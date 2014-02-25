d5b5a58e95dee187f3053608adf2e072fc544cbd
agn0605 <aney@mail.umw.edu>
2014-02-25 10:05:11 -0500

    compliments.sql
    This is just a structure for the final product, which will be finished during break.

---
+++ compliments.sql
@@ -0,0 +1,70 @@
+DROP DATABASE IF EXISTS complimentGenerator
+CREATE DATABASE IF NOT EXISTS complimentGenerator
+USE complimentGenerator
+
+--
+--Table structure for table "compliment"
+--
+
+DROP TABLE IF EXISTS 'compliment'
+CREATE TABLE 'compliment' (
+  `ID` int(11) NOT NULL auto_increment,
+  'Noun' varchar(35) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  'Adjective' varchar(35) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  PRIMARY KEY  (`ID`)
+) ;
+
+--
+--Building data for table "compliment"
+--
+
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
+INSERT INTO abductions VALUES ('noun', 'adjective');
