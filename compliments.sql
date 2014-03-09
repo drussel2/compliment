b73b195d9838a98c4265ba4aa8d65f31ac1be6ad
agn0605 <aney@mail.umw.edu>
2014-03-09 19:17:40 -0400

    FINISHED

--- compliments.sql
+++ compliments.sql
@@ -1,5 +1,8 @@
-DROP DATABASE IF EXISTS complimentGenerator
-CREATE DATABASE IF NOT EXISTS complimentGenerator
+DROP DATABASE IF EXISTS complimentDB
+CREATE DATABASE IF NOT EXISTS complimentDB
+GRANT ALL PRIVILEGES ON complimentDB.* to 'compliment'@'localhost' 
+identified by 'password';
+USE complimentDB;
 USE complimentGenerator
 
 --
@@ -9,7 +12,6 @@ USE complimentGenerator
 DROP TABLE IF EXISTS 'compliment'
 CREATE TABLE 'compliment' (
   `ID` int(11) NOT NULL auto_increment,
-  'Noun' varchar(35) CHARACTER SET utf8 NOT NULL DEFAULT '',
   'Adjective' varchar(35) CHARACTER SET utf8 NOT NULL DEFAULT '',
   PRIMARY KEY  (`ID`)
 ) ;
@@ -18,53 +20,53 @@ CREATE TABLE 'compliment' (
 --Building data for table "compliment"
 --
 
-INSERT INTO abductions VALUES ('noun', 'awesome');
-INSERT INTO abductions VALUES ('noun', 'amazing');
-INSERT INTO abductions VALUES ('noun', 'beautiful');
-INSERT INTO abductions VALUES ('noun', 'brilliant');
-INSERT INTO abductions VALUES ('noun', 'charming');
-INSERT INTO abductions VALUES ('noun', 'cool');
-INSERT INTO abductions VALUES ('noun', 'dazzling');
-INSERT INTO abductions VALUES ('noun', 'delightful');
-INSERT INTO abductions VALUES ('noun', 'electrifying');
-INSERT INTO abductions VALUES ('noun', 'exquisite');
-INSERT INTO abductions VALUES ('noun', 'fabulous');
-INSERT INTO abductions VALUES ('noun', 'fun');
-INSERT INTO abductions VALUES ('noun', 'good');
-INSERT INTO abductions VALUES ('noun', 'great');
-INSERT INTO abductions VALUES ('noun', 'handsome');
-INSERT INTO abductions VALUES ('noun', 'harmonious');
-INSERT INTO abductions VALUES ('noun', 'ideal');
-INSERT INTO abductions VALUES ('noun', 'impressive');
-INSERT INTO abductions VALUES ('noun', 'joyous');
-INSERT INTO abductions VALUES ('noun', 'jubilant');
-INSERT INTO abductions VALUES ('noun', 'keen');
-INSERT INTO abductions VALUES ('noun', 'kind');
-INSERT INTO abductions VALUES ('noun', 'legendary');
-INSERT INTO abductions VALUES ('noun', 'luminous');
-INSERT INTO abductions VALUES ('noun', 'marvelous');
-INSERT INTO abductions VALUES ('noun', 'meritorious');
-INSERT INTO abductions VALUES ('noun', 'nice');
-INSERT INTO abductions VALUES ('noun', 'novel');
-INSERT INTO abductions VALUES ('noun', 'okay');
-INSERT INTO abductions VALUES ('noun', 'one-hundred percent');
-INSERT INTO abductions VALUES ('noun', 'perfect');
-INSERT INTO abductions VALUES ('noun', 'phenomenal');
-INSERT INTO abductions VALUES ('noun', 'quality');
-INSERT INTO abductions VALUES ('noun', 'quick');
-INSERT INTO abductions VALUES ('noun', 'refreshing');
-INSERT INTO abductions VALUES ('noun', 'remarkable');
-INSERT INTO abductions VALUES ('noun', 'satisfactory');
-INSERT INTO abductions VALUES ('noun', 'sparkling');
-INSERT INTO abductions VALUES ('noun', 'terrific');
-INSERT INTO abductions VALUES ('noun', 'thrilling');
-INSERT INTO abductions VALUES ('noun', 'unreal');
-INSERT INTO abductions VALUES ('noun', 'upbeat');
-INSERT INTO abductions VALUES ('noun', 'vibrant');
-INSERT INTO abductions VALUES ('noun', 'virtuous');
-INSERT INTO abductions VALUES ('noun', 'wonderful');
-INSERT INTO abductions VALUES ('noun', 'wondrous');
-INSERT INTO abductions VALUES ('noun', 'adorable');
-INSERT INTO abductions VALUES ('noun', 'excellent');
-INSERT INTO abductions VALUES ('noun', 'agreeable');
-INSERT INTO abductions VALUES ('noun', 'gorgeous');
+INSERT INTO abductions VALUES ('awesome');
+INSERT INTO abductions VALUES ('amazing');
+INSERT INTO abductions VALUES ('beautiful');
+INSERT INTO abductions VALUES ('brilliant');
+INSERT INTO abductions VALUES ('charming');
+INSERT INTO abductions VALUES ('cool');
+INSERT INTO abductions VALUES ('dazzling');
+INSERT INTO abductions VALUES ('delightful');
+INSERT INTO abductions VALUES ('electrifying');
+INSERT INTO abductions VALUES ('exquisite');
+INSERT INTO abductions VALUES ('fabulous');
+INSERT INTO abductions VALUES ('fun');
+INSERT INTO abductions VALUES ('good');
+INSERT INTO abductions VALUES ('great');
+INSERT INTO abductions VALUES ('handsome');
+INSERT INTO abductions VALUES ('harmonious');
+INSERT INTO abductions VALUES ('ideal');
+INSERT INTO abductions VALUES ('impressive');
+INSERT INTO abductions VALUES ('joyous');
+INSERT INTO abductions VALUES ('jubilant');
+INSERT INTO abductions VALUES ('keen');
+INSERT INTO abductions VALUES ('kind');
+INSERT INTO abductions VALUES ('legendary');
+INSERT INTO abductions VALUES ('luminous');
+INSERT INTO abductions VALUES ('marvelous');
+INSERT INTO abductions VALUES ('meritorious');
+INSERT INTO abductions VALUES ('nice');
+INSERT INTO abductions VALUES ('novel');
+INSERT INTO abductions VALUES ('okay');
+INSERT INTO abductions VALUES ('one-hundred percent');
+INSERT INTO abductions VALUES ('perfect');
+INSERT INTO abductions VALUES ('phenomenal');
+INSERT INTO abductions VALUES ('quality');
+INSERT INTO abductions VALUES ('quick');
+INSERT INTO abductions VALUES ('refreshing');
+INSERT INTO abductions VALUES ('remarkable');
+INSERT INTO abductions VALUES ('satisfactory');
+INSERT INTO abductions VALUES ('sparkling');
+INSERT INTO abductions VALUES ('terrific');
+INSERT INTO abductions VALUES ('thrilling');
+INSERT INTO abductions VALUES ('unreal');
+INSERT INTO abductions VALUES ('upbeat');
+INSERT INTO abductions VALUES ('vibrant');
+INSERT INTO abductions VALUES ('virtuous');
+INSERT INTO abductions VALUES ('wonderful');
+INSERT INTO abductions VALUES ('wondrous');
+INSERT INTO abductions VALUES ('adorable');
+INSERT INTO abductions VALUES ('excellent');
+INSERT INTO abductions VALUES ('agreeable');
+INSERT INTO abductions VALUES ('gorgeous');
