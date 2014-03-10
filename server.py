c42f0697b9ff87b253dc717e097583e153cbb92d
vertyKolid <egreene@mail.umw.edu>
2014-03-10 12:09:36 -0400

    Update server.py

--- server.py
+++ server.py
@@ -13,15 +13,30 @@ def about():
     page = 'about'
     return render_template('about.html', page=page)
   
-@app.route('/compliment')
+@app.route('/compliment', methods=['GET', 'POST'])
 def compliment():
-  db = complimentutil.db_connect()
-  cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-  randomnum = randint(1,50) #Inclusive
-  query = "SELECT Adjective FROM words WHERE ID = '%s'" %randomnum
-  cur.execute(query)
-  db.commit()
-  return redirect(url_for('compliment.html'))
+  page = 'compliment'
+  getCompliment = False
+  
+  
+  if request.method == 'POST':
+    getCompliment = True
+    db = complimentutil.db_connect()
+    cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+    randomnum = randint(1,50) #Inclusive
+    query = "SELECT Adjective FROM words WHERE ID = '%s'" %randomnum
+    cur.execute(query)   
+    rows = cur.fetchone()
+    return render_template('compliment.html', page=page, getCompliment=getCompliment, rows=rows, randomnum=randomnum)
+  return render_template('compliment.html', page=page, getCompliment=getCompliment)
+#return redirect(url_for('compliment.html'))
+
+@app.route('/blog')
+def blog():
+    page = 'blog'
+    return render_template('blog.html', page=page)
+  
+
   
 if __name__ == '__main__':
     app.debug=True
