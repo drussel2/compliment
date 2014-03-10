4c61c98af2b1b83d8c36a79c9d79d6cc40725f95
agn0605 <aney@mail.umw.edu>
2014-03-09 21:57:40 -0400

    Update compliments.sql

--- compliments.sql
+++ compliments.sql
@@ -1,70 +1,64 @@
-DROP DATABASE complimentDB
-CREATE DATABASE IF NOT EXISTS complimentDB
+DROP DATABASE complimentDB;
+
+CREATE DATABASE IF NOT EXISTS complimentDB;
 GRANT ALL PRIVILEGES ON complimentDB.* to 'compliment'@'localhost' 
 identified by 'password';
 USE complimentDB;
 
---
---Table structure for table "compliment"
---
+CREATE TABLE words
+(
+  ID INT NOT NULL AUTO_INCREMENT,
+  Adjective VARCHAR(35),
+  PRIMARY KEY (ID)
+);
 
-CREATE TABLE 'compliment' (
-  `ID` int(11) NOT NULL auto_increment,
-  'Adjective' varchar(35) CHARACTER SET utf8 NOT NULL DEFAULT '',
-  PRIMARY KEY  (`ID`)
-) ;
-
---
---Building data for table "compliment"
---
-
-INSERT INTO compliment ('Adjective') VALUES ('awesome');
-INSERT INTO compliment ('Adjective') VALUES ('amazing');
-INSERT INTO compliment ('Adjective') VALUES ('beautiful');
-INSERT INTO compliment ('Adjective') VALUES ('brilliant');
-INSERT INTO compliment ('Adjective') VALUES ('charming');
-INSERT INTO compliment ('Adjective') VALUES ('cool');
-INSERT INTO compliment ('Adjective') VALUES ('dazzling');
-INSERT INTO compliment ('Adjective') VALUES ('delightful');
-INSERT INTO compliment ('Adjective') VALUES ('electrifying');
-INSERT INTO compliment ('Adjective') VALUES ('exquisite');
-INSERT INTO compliment ('Adjective') VALUES ('fabulous');
-INSERT INTO compliment ('Adjective') VALUES ('fun');
-INSERT INTO compliment ('Adjective') VALUES ('good');
-INSERT INTO compliment ('Adjective') VALUES ('great');
-INSERT INTO compliment ('Adjective') VALUES ('handsome');
-INSERT INTO compliment ('Adjective') VALUES ('harmonious');
-INSERT INTO compliment ('Adjective') VALUES ('ideal');
-INSERT INTO compliment ('Adjective') VALUES ('impressive');
-INSERT INTO compliment ('Adjective') VALUES ('joyous');
-INSERT INTO compliment ('Adjective') VALUES ('jubilant');
-INSERT INTO compliment ('Adjective') VALUES ('keen');
-INSERT INTO compliment ('Adjective') VALUES ('kind');
-INSERT INTO compliment ('Adjective') VALUES ('legendary');
-INSERT INTO compliment ('Adjective') VALUES ('luminous');
-INSERT INTO compliment ('Adjective') VALUES ('marvelous');
-INSERT INTO compliment ('Adjective') VALUES ('meritorious');
-INSERT INTO compliment ('Adjective') VALUES ('nice');
-INSERT INTO compliment ('Adjective') VALUES ('novel');
-INSERT INTO compliment ('Adjective') VALUES ('okay');
-INSERT INTO compliment ('Adjective') VALUES ('one-hundred percent');
-INSERT INTO compliment ('Adjective') VALUES ('perfect');
-INSERT INTO compliment ('Adjective') VALUES ('phenomenal');
-INSERT INTO compliment ('Adjective') VALUES ('quality');
-INSERT INTO compliment ('Adjective') VALUES ('quick');
-INSERT INTO compliment ('Adjective') VALUES ('refreshing');
-INSERT INTO compliment ('Adjective') VALUES ('remarkable');
-INSERT INTO compliment ('Adjective') VALUES ('satisfactory');
-INSERT INTO compliment ('Adjective') VALUES ('sparkling');
-INSERT INTO compliment ('Adjective') VALUES ('terrific');
-INSERT INTO compliment ('Adjective') VALUES ('thrilling');
-INSERT INTO compliment ('Adjective') VALUES ('unreal');
-INSERT INTO compliment ('Adjective') VALUES ('upbeat');
-INSERT INTO compliment ('Adjective') VALUES ('vibrant');
-INSERT INTO compliment ('Adjective') VALUES ('virtuous');
-INSERT INTO compliment ('Adjective') VALUES ('wonderful');
-INSERT INTO compliment ('Adjective') VALUES ('wondrous');
-INSERT INTO compliment ('Adjective') VALUES ('adorable');
-INSERT INTO compliment ('Adjective') VALUES ('excellent');
-INSERT INTO compliment ('Adjective') VALUES ('agreeable');
-INSERT INTO compliment ('Adjective') VALUES ('gorgeous');
+INSERT INTO words (Adjective) VALUES ('awesome');
+INSERT INTO words (Adjective) VALUES ('amazing');
+INSERT INTO words (Adjective) VALUES ('beautiful');
+INSERT INTO words (Adjective) VALUES ('brilliant');
+INSERT INTO words (Adjective) VALUES ('charming');
+INSERT INTO words (Adjective) VALUES ('cool');
+INSERT INTO words (Adjective) VALUES ('dazzling');
+INSERT INTO words (Adjective) VALUES ('delightful');
+INSERT INTO words (Adjective) VALUES ('electrifying');
+INSERT INTO words (Adjective) VALUES ('exquisite');
+INSERT INTO words (Adjective) VALUES ('fabulous');
+INSERT INTO words (Adjective) VALUES ('fun');
+INSERT INTO words (Adjective) VALUES ('good');
+INSERT INTO words (Adjective) VALUES ('great');
+INSERT INTO words (Adjective) VALUES ('handsome');
+INSERT INTO words (Adjective) VALUES ('harmonious');
+INSERT INTO words (Adjective) VALUES ('ideal');
+INSERT INTO words (Adjective) VALUES ('impressive');
+INSERT INTO words (Adjective) VALUES ('joyous');
+INSERT INTO words (Adjective) VALUES ('jubilant');
+INSERT INTO words (Adjective) VALUES ('keen');
+INSERT INTO words (Adjective) VALUES ('kind');
+INSERT INTO words (Adjective) VALUES ('legendary');
+INSERT INTO words (Adjective) VALUES ('luminous');
+INSERT INTO words (Adjective) VALUES ('marvelous');
+INSERT INTO words (Adjective) VALUES ('meritorious');
+INSERT INTO words (Adjective) VALUES ('nice');
+INSERT INTO words (Adjective) VALUES ('novel');
+INSERT INTO words (Adjective) VALUES ('okay');
+INSERT INTO words (Adjective) VALUES ('one-hundred percent');
+INSERT INTO words (Adjective) VALUES ('perfect');
+INSERT INTO words (Adjective) VALUES ('phenomenal');
+INSERT INTO words (Adjective) VALUES ('quality');
+INSERT INTO words (Adjective) VALUES ('quick');
+INSERT INTO words (Adjective) VALUES ('refreshing');
+INSERT INTO words (Adjective) VALUES ('remarkable');
+INSERT INTO words (Adjective) VALUES ('satisfactory');
+INSERT INTO words (Adjective) VALUES ('sparkling');
+INSERT INTO words (Adjective) VALUES ('terrific');
+INSERT INTO words (Adjective) VALUES ('thrilling');
+INSERT INTO words (Adjective) VALUES ('unreal');
+INSERT INTO words (Adjective) VALUES ('upbeat');
+INSERT INTO words (Adjective) VALUES ('vibrant');
+INSERT INTO words (Adjective) VALUES ('virtuous');
+INSERT INTO words (Adjective) VALUES ('wonderful');
+INSERT INTO words (Adjective) VALUES ('wondrous');
+INSERT INTO words (Adjective) VALUES ('adorable');
+INSERT INTO words (Adjective) VALUES ('excellent');
+INSERT INTO words (Adjective) VALUES ('agreeable');
+INSERT INTO words (Adjective) VALUES ('gorgeous');
