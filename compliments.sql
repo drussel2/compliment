ac66bc905b97b6a7bfb8455001a27f5dcce9025f
agn0605 <aney@mail.umw.edu>
2014-03-09 21:02:49 -0400

    Update compliments.sql

--- compliments.sql
+++ compliments.sql
@@ -1,15 +1,13 @@
-DROP DATABASE IF EXISTS complimentDB
+DROP DATABASE complimentDB
 CREATE DATABASE IF NOT EXISTS complimentDB
 GRANT ALL PRIVILEGES ON complimentDB.* to 'compliment'@'localhost' 
 identified by 'password';
 USE complimentDB;
-USE complimentGenerator
 
 --
 --Table structure for table "compliment"
 --
 
-DROP TABLE IF EXISTS 'compliment'
 CREATE TABLE 'compliment' (
   `ID` int(11) NOT NULL auto_increment,
   'Adjective' varchar(35) CHARACTER SET utf8 NOT NULL DEFAULT '',
@@ -20,53 +18,53 @@ CREATE TABLE 'compliment' (
 --Building data for table "compliment"
 --
 
-INSERT INTO abductions VALUES ('awesome');
-INSERT INTO abductions VALUES ('amazing');
-INSERT INTO abductions VALUES ('beautiful');
-INSERT INTO abductions VALUES ('brilliant');
-INSERT INTO abductions VALUES ('charming');
-INSERT INTO abductions VALUES ('cool');
-INSERT INTO abductions VALUES ('dazzling');
-INSERT INTO abductions VALUES ('delightful');
-INSERT INTO abductions VALUES ('electrifying');
-INSERT INTO abductions VALUES ('exquisite');
-INSERT INTO abductions VALUES ('fabulous');
-INSERT INTO abductions VALUES ('fun');
-INSERT INTO abductions VALUES ('good');
-INSERT INTO abductions VALUES ('great');
-INSERT INTO abductions VALUES ('handsome');
-INSERT INTO abductions VALUES ('harmonious');
-INSERT INTO abductions VALUES ('ideal');
-INSERT INTO abductions VALUES ('impressive');
-INSERT INTO abductions VALUES ('joyous');
-INSERT INTO abductions VALUES ('jubilant');
-INSERT INTO abductions VALUES ('keen');
-INSERT INTO abductions VALUES ('kind');
-INSERT INTO abductions VALUES ('legendary');
-INSERT INTO abductions VALUES ('luminous');
-INSERT INTO abductions VALUES ('marvelous');
-INSERT INTO abductions VALUES ('meritorious');
-INSERT INTO abductions VALUES ('nice');
-INSERT INTO abductions VALUES ('novel');
-INSERT INTO abductions VALUES ('okay');
-INSERT INTO abductions VALUES ('one-hundred percent');
-INSERT INTO abductions VALUES ('perfect');
-INSERT INTO abductions VALUES ('phenomenal');
-INSERT INTO abductions VALUES ('quality');
-INSERT INTO abductions VALUES ('quick');
-INSERT INTO abductions VALUES ('refreshing');
-INSERT INTO abductions VALUES ('remarkable');
-INSERT INTO abductions VALUES ('satisfactory');
-INSERT INTO abductions VALUES ('sparkling');
-INSERT INTO abductions VALUES ('terrific');
-INSERT INTO abductions VALUES ('thrilling');
-INSERT INTO abductions VALUES ('unreal');
-INSERT INTO abductions VALUES ('upbeat');
-INSERT INTO abductions VALUES ('vibrant');
-INSERT INTO abductions VALUES ('virtuous');
-INSERT INTO abductions VALUES ('wonderful');
-INSERT INTO abductions VALUES ('wondrous');
-INSERT INTO abductions VALUES ('adorable');
-INSERT INTO abductions VALUES ('excellent');
-INSERT INTO abductions VALUES ('agreeable');
-INSERT INTO abductions VALUES ('gorgeous');
+INSERT INTO compliment ('Adjective') VALUES ('awesome');
+INSERT INTO compliment ('Adjective') VALUES ('amazing');
+INSERT INTO compliment ('Adjective') VALUES ('beautiful');
+INSERT INTO compliment ('Adjective') VALUES ('brilliant');
+INSERT INTO compliment ('Adjective') VALUES ('charming');
+INSERT INTO compliment ('Adjective') VALUES ('cool');
+INSERT INTO compliment ('Adjective') VALUES ('dazzling');
+INSERT INTO compliment ('Adjective') VALUES ('delightful');
+INSERT INTO compliment ('Adjective') VALUES ('electrifying');
+INSERT INTO compliment ('Adjective') VALUES ('exquisite');
+INSERT INTO compliment ('Adjective') VALUES ('fabulous');
+INSERT INTO compliment ('Adjective') VALUES ('fun');
+INSERT INTO compliment ('Adjective') VALUES ('good');
+INSERT INTO compliment ('Adjective') VALUES ('great');
+INSERT INTO compliment ('Adjective') VALUES ('handsome');
+INSERT INTO compliment ('Adjective') VALUES ('harmonious');
+INSERT INTO compliment ('Adjective') VALUES ('ideal');
+INSERT INTO compliment ('Adjective') VALUES ('impressive');
+INSERT INTO compliment ('Adjective') VALUES ('joyous');
+INSERT INTO compliment ('Adjective') VALUES ('jubilant');
+INSERT INTO compliment ('Adjective') VALUES ('keen');
+INSERT INTO compliment ('Adjective') VALUES ('kind');
+INSERT INTO compliment ('Adjective') VALUES ('legendary');
+INSERT INTO compliment ('Adjective') VALUES ('luminous');
+INSERT INTO compliment ('Adjective') VALUES ('marvelous');
+INSERT INTO compliment ('Adjective') VALUES ('meritorious');
+INSERT INTO compliment ('Adjective') VALUES ('nice');
+INSERT INTO compliment ('Adjective') VALUES ('novel');
+INSERT INTO compliment ('Adjective') VALUES ('okay');
+INSERT INTO compliment ('Adjective') VALUES ('one-hundred percent');
+INSERT INTO compliment ('Adjective') VALUES ('perfect');
+INSERT INTO compliment ('Adjective') VALUES ('phenomenal');
+INSERT INTO compliment ('Adjective') VALUES ('quality');
+INSERT INTO compliment ('Adjective') VALUES ('quick');
+INSERT INTO compliment ('Adjective') VALUES ('refreshing');
+INSERT INTO compliment ('Adjective') VALUES ('remarkable');
+INSERT INTO compliment ('Adjective') VALUES ('satisfactory');
+INSERT INTO compliment ('Adjective') VALUES ('sparkling');
+INSERT INTO compliment ('Adjective') VALUES ('terrific');
+INSERT INTO compliment ('Adjective') VALUES ('thrilling');
+INSERT INTO compliment ('Adjective') VALUES ('unreal');
+INSERT INTO compliment ('Adjective') VALUES ('upbeat');
+INSERT INTO compliment ('Adjective') VALUES ('vibrant');
+INSERT INTO compliment ('Adjective') VALUES ('virtuous');
+INSERT INTO compliment ('Adjective') VALUES ('wonderful');
+INSERT INTO compliment ('Adjective') VALUES ('wondrous');
+INSERT INTO compliment ('Adjective') VALUES ('adorable');
+INSERT INTO compliment ('Adjective') VALUES ('excellent');
+INSERT INTO compliment ('Adjective') VALUES ('agreeable');
+INSERT INTO compliment ('Adjective') VALUES ('gorgeous');
