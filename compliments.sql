865deadfaeed331362f14c2b4d77344f11eb782f
agn0605 <aney@mail.umw.edu>
2014-03-30 16:13:09 -0400

    Update compliments.sql

--- compliments.sql
+++ compliments.sql
@@ -13,117 +13,132 @@ CREATE TABLE words
   PRIMARY KEY (ID)
 );
 
-INSERT INTO words (Adjective) VALUES ('awesome', 'T');
-INSERT INTO words (Adjective) VALUES ('amazing', 'T');
-INSERT INTO words (Adjective) VALUES ('beautiful', 'T');
-INSERT INTO words (Adjective) VALUES ('brilliant', 'T');
-INSERT INTO words (Adjective) VALUES ('charming', 'T');
-INSERT INTO words (Adjective) VALUES ('cool', 'T');
-INSERT INTO words (Adjective) VALUES ('dazzling', 'T');
-INSERT INTO words (Adjective) VALUES ('delightful', 'T');
-INSERT INTO words (Adjective) VALUES ('electrifying', 'T');
-INSERT INTO words (Adjective) VALUES ('exquisite', 'T');
-INSERT INTO words (Adjective) VALUES ('fabulous', 'T');
-INSERT INTO words (Adjective) VALUES ('fun', 'T');
-INSERT INTO words (Adjective) VALUES ('good', 'T');
-INSERT INTO words (Adjective) VALUES ('great', 'T');
-INSERT INTO words (Adjective) VALUES ('handsome', 'T');
-INSERT INTO words (Adjective) VALUES ('harmonious', 'T');
-INSERT INTO words (Adjective) VALUES ('ideal', 'T');
-INSERT INTO words (Adjective) VALUES ('impressive', 'T');
-INSERT INTO words (Adjective) VALUES ('joyous', 'T');
-INSERT INTO words (Adjective) VALUES ('jubilant', 'T');
-INSERT INTO words (Adjective) VALUES ('keen', 'T');
-INSERT INTO words (Adjective) VALUES ('kind', 'T');
-INSERT INTO words (Adjective) VALUES ('legendary', 'T');
-INSERT INTO words (Adjective) VALUES ('luminous', 'T');
-INSERT INTO words (Adjective) VALUES ('marvelous', 'T');
-INSERT INTO words (Adjective) VALUES ('meritorious', 'T');
-INSERT INTO words (Adjective) VALUES ('nice', 'T');
-INSERT INTO words (Adjective) VALUES ('novel', 'T');
-INSERT INTO words (Adjective) VALUES ('okay', 'T');
-INSERT INTO words (Adjective) VALUES ('perfect', 'T');
-INSERT INTO words (Adjective) VALUES ('phenomenal', 'T');
-INSERT INTO words (Adjective) VALUES ('incredible', 'T');
-INSERT INTO words (Adjective) VALUES ('smashing', 'T');
-INSERT INTO words (Adjective) VALUES ('refreshing', 'T');
-INSERT INTO words (Adjective) VALUES ('remarkable', 'T');
-INSERT INTO words (Adjective) VALUES ('satisfactory', 'T');
-INSERT INTO words (Adjective) VALUES ('sparkling', 'T');
-INSERT INTO words (Adjective) VALUES ('terrific', 'T');
-INSERT INTO words (Adjective) VALUES ('thrilling', 'T');
-INSERT INTO words (Adjective) VALUES ('unreal', 'T');
-INSERT INTO words (Adjective) VALUES ('upbeat', 'T');
-INSERT INTO words (Adjective) VALUES ('vibrant', 'T');
-INSERT INTO words (Adjective) VALUES ('virtuous', 'T');
-INSERT INTO words (Adjective) VALUES ('wonderful', 'T');
-INSERT INTO words (Adjective) VALUES ('wondrous', 'T');
-INSERT INTO words (Adjective) VALUES ('adorable', 'T');
-INSERT INTO words (Adjective) VALUES ('excellent', 'T');
-INSERT INTO words (Adjective) VALUES ('agreeable', 'T');
-INSERT INTO words (Adjective) VALUES ('gorgeous', 'T');
-INSERT INTO words (Adjective) VALUES ('splendiferous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('awesome', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('amazing', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('beautiful', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('brilliant', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('charming', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('cool', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('dazzling', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('delightful', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('electrifying', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('exquisite', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('fabulous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('fun', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('good', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('great', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('handsome', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('harmonious', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('ideal', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('impressive', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('joyous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('jubilant', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('keen', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('kind', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('legendary', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('luminous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('marvelous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('meritorious', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('nice', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('novel', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('okay', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('perfect', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('phenomenal', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('incredible', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('smashing', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('refreshing', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('remarkable', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('satisfactory', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('sparkling', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('terrific', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('thrilling', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('unreal', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('upbeat', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('vibrant', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('virtuous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('wonderful', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('wondrous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('adorable', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('excellent', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('agreeable', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('gorgeous', 'F');
+INSERT INTO words (Adjective, sarVal) VALUES ('trite', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('banal', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('unengaging', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('childish', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('annoying', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('ungrateful', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('substandard', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('incompetent', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('uncouth', 'T');
+INSERT INTO words (Adjective, sarVal) VALUES ('skanky', 'T');
 
 CREATE TABLE adverbs_vowels
 (
   ID INT NOT NULL AUTO_INCREMENT,
   Adverb VARCHAR(35),
+  Intensity INT(1),
   PRIMARY KEY (ID)
 );
 
-INSERT INTO adverbs_vowels (Adverb) VALUES ('awesomely');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('amazingly');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('electrifyingly');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('exquisitely');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('ideally');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('impressively');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('adorably');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('excellently');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('agreeably');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('unbelievably');
-INSERT INTO adverbs_vowels (Adverb) VALUES ('extremely');
+CREATE INDEX intensity ON adverbs_vowels (Adverb, Intensity);
+
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('awesomely', 1);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('amazingly', 1);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('electrifyingly', 3);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('exquisitely', 2);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('ideally', 2);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('impressively', 2);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('adorably', 1);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('excellently', 2);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('agreeably', 1);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('unbelievably', 3);
+INSERT INTO adverbs_vowels (Adverb, Intensity) VALUES ('extremely', 2);
 
 CREATE TABLE adverbs_consonant
 (
   ID INT NOT NULL AUTO_INCREMENT,
   Adverb VARCHAR(35),
+  Intensity INT(1),
   PRIMARY KEY (ID)
 );
 
-INSERT INTO adverbs_consonant (Adverb) VALUES ('beautifully');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('brilliantly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('charmingly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('dazzlingly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('delightfully');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('fabulously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('greatly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('handsomely');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('harmoniously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('joyously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('jubilantly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('keenly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('kindly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('legendarily');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('luminously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('marvelously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('meritoriously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('novelly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('perfectly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('phenomenally');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('incredibly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('smashingly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('refreshingly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('remarkably');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('satisfactorily');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('sparklingly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('terrifically');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('thrillingly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('vibrantly');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('virtuously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('wonderfully');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('wondrously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('gorgeously');
-INSERT INTO adverbs_consonant (Adverb) VALUES ('bodaciously');
+CREATE INDEX intensity ON adverbs_consonant (Adverb, Intensity);
+
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('beautifully', 3);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('brilliantly', 3);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('charmingly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('dazzlingly', 3);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('delightfully', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('fabulously', 3);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('greatly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('handsomely', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('harmoniously', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('joyously', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('jubilantly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('keenly', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('kindly', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('legendarily', 3);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('luminously', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('marvelously', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('meritoriously', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('novelly', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('perfectly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('phenomenally', 3);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('incredibly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('smashingly', 3);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('refreshingly', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('remarkably', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('satisfactorily', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('sparklingly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('terrifically', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('thrillingly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('vibrantly', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('virtuously', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('wonderfully', 1);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('wondrously', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('gorgeously', 2);
+INSERT INTO adverbs_consonant (Adverb, Intensity) VALUES ('bodaciously', 3);
 
 CREATE TABLE proper_grammar
 (
